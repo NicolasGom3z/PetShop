e218bcece7e851dc10a0dfe3101fb492e3052a17
Nicolas Gomez <nicholasgom3z@hotmail.com>
2021-02-24 15:30:57 -0500

    - Se agrego animacion de background al header junto con la navegacion. - Se corrigio el error de bubbling

--- js/script.js
+++ js/script.js
@@ -11,12 +11,21 @@ burger.addEventListener('click', accionBurger);
 
 // FUNCIONES
 
-function accionBurger(){
-  if(burgerState === false){
-    burger.nextElementSibling.classList.add('slide-rigth');
-    burgerState = true;
-  }else {
-    burger.nextElementSibling.classList.remove('slide-rigth');    
-    burgerState = false;
+function accionBurger(e){
+
+  
+  if(e.target.nodeName === 'I' || e.target.nodeName === 'i'){
+    
+    if(burgerState === false){
+      burger.nextElementSibling.classList.add('slide-rigth');
+      burger.parentElement.classList.add('header-act');
+      burgerState = true;
+    }else {
+      burger.nextElementSibling.classList.remove('slide-rigth'); 
+      burger.parentElement.classList.remove('header-act')   
+      burgerState = false;
+    }
   }
+
+  
 }
